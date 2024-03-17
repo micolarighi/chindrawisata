470f7bc44987624c703c1c869871ba777fd7c2ab
paradoxicalinsomniac <81902126+micolarighi@users.noreply.github.com>
2024-03-17 21:16:48 +0700

    fix icon

--- src/routes/components/WhyUs.svelte
+++ src/routes/components/WhyUs.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
 <div class="text-center mb-8 mt-16 bg-zinc-100 px-8 md:px-24 py-12">
 	<h1 class="md:text-4xl text-2xl font-bold">Kenapa Memilih Kami?</h1>
 	<p class="text-gray-400 mb-8 mt-2 leading-none">
@@ -5,6 +9,9 @@
 	</p>
 	<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
 		<div class="card bg-base-100 shadow-xl">
+			<div class="text-center flex justify-center mt-5">
+				<Icon icon="solar:tag-price-bold" class="text-6xl text-lime-400" />
+			</div>
 			<div class="card-body">
 				<h2 class="text-2xl font-bold text-center">Harga Terjangkau</h2>
 				<p>Dapatkan harga paket perjalanan murah dengan penawaran menarik.</p>
@@ -12,6 +19,9 @@
 			</div>
 		</div>
 		<div class="card bg-base-100 shadow-xl">
+			<div class="text-center flex justify-center mt-5">
+				<Icon icon="fa6-solid:handshake" class="text-6xl text-lime-400" />
+			</div>
 			<div class="card-body">
 				<h2 class="text-2xl font-bold text-center">Terpercaya</h2>
 				<p>Mendapatkan kepercayaan dari konsumen yang memilih kami.</p>
@@ -19,6 +29,9 @@
 			</div>
 		</div>
 		<div class="card bg-base-100 shadow-xl">
+			<div class="text-center flex justify-center mt-5">
+				<Icon icon="mdi:email-fast" class="text-6xl text-lime-400" />
+			</div>
 			<div class="card-body">
 				<h2 class="text-2xl font-bold text-center">Pemesanan Mudah</h2>
 				<p>Sistem pemesanan yang mudah dengan proses yang sederhana.</p>

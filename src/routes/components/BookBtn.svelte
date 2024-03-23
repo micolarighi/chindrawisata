60ab425b5a9cbfaf7578daef989f39853b8de793
paradoxicalinsomniac <81902126+micolarighi@users.noreply.github.com>
2024-03-24 01:28:46 +0700

    minor fixes

--- src/routes/components/BookBtn.svelte
+++ src/routes/components/BookBtn.svelte
@@ -11,6 +11,7 @@
 {:else}
 	<a
 		href="https://wa.me/6281280803303?text=Assalamu'alaikum,%20Chindra Wisata%20Saya%20mau%20tanya-tanya%20soal%20travel."
+		target="_blank"
 		class="btn bg-lime-400 text-xs md:text-md">Pesan Sekarang</a
 	>
 {/if}
